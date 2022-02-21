64a0cbace69d4a1e89159ae9f8adfc706f0cda07
Juan Carlos <jchernandez827@gmail.com>
2022-02-21 17:15:09 -0500

    Create queries for the models, update ApplicationController with current_user method

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,10 @@
 class Comment < ApplicationRecord
   belongs_to :post
   belongs_to :author, class_name: 'User'
+
+  def self.update_comments_counter(post)
+    counter = Comment.where("post_id = #{post.id}").count
+    post.comments_counter = counter
+    post.save
+  end
 end
--- app/models/like.rb
+++ app/models/like.rb
@@ -1,4 +1,10 @@
 class Like < ApplicationRecord
   belongs_to :post
   belongs_to :author, class_name: 'User'
+
+  def self.update_likes_counter(post)
+    counter = Like.where("post_id = #{post.id}").count
+    post.likes_counter = counter
+    post.save
+  end
 end

3fb159bee00d633a9df88e387ada73f5887a9900
Juan Carlos <jchernandez827@gmail.com>
2022-02-25 19:09:53 -0500

    Add flash msg to Posts, and Comments controller create actions

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -9,8 +9,10 @@ class CommentsController < ApplicationController
     @comment.author_id = params[:user_id]
     @comment.post_id = params[:post_id]
     if @comment.save
-      redirect_to user_post_path(id: params[:post_id], user_id: params[:user_id])
+      flash[:success] = 'Comment created successfully'
+      redirect_to user_posts_path(user_id: params[:user_id])
     else
+      flash.now[:error] = 'Error: Comment could not be create'
       render :new
     end
   end
